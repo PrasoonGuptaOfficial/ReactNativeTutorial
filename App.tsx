55d8da40ba34bf3caf39eed1b6eb6f48798bb5d0
Prasoon Gupta <PrasoonguptaOfficial@gmail.com>
2024-03-14 15:55:56 +0530

    All Basic Component

--- App.tsx
+++ App.tsx
@@ -1,10 +1,77 @@
-import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import React, {useState} from 'react';
+import {
+  Text,
+  View,
+  StyleSheet,
+  Image,
+  TextInput,
+  ScrollView,
+} from 'react-native';
 
 function App(): React.JSX.Element {
+  const [value, setText] = useState('');
   return (
     <View>
       <Text style={styles.textBold}>ABC</Text>
+      <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+      <TextInput
+        value={value}
+        onChangeText={setText}
+        placeholder="Enter String"
+      />
+      <ScrollView>
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+        <Image source={require('./Tutorial.png')} style={styles.imageSize} />
+      </ScrollView>
     </View>
   );
 }
@@ -13,6 +80,10 @@ const styles = StyleSheet.create({
   textBold: {
     color: 'red',
     fontSize: 20,
+  },
+  imageSize: {
+    width: 20,
+    height: 20,
   },
 });
 
