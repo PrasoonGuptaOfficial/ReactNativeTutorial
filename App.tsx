248d97d89250cb0d293de37be88d30cd4bea3e83
Prasoon Gupta <PrasoonguptaOfficial@gmail.com>
2024-03-14 15:37:19 +0530

    Text Basic Component

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
-import {View} from 'react-native';
+import {Text, View} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
-    <View style={{height: '100%'}}>
-      <View style={{backgroundColor: 'red', flex: 1}} />
-      <View style={{backgroundColor: 'yellow', flex: 2}} />
-      <View style={{backgroundColor: 'orange', flex: 3}} />
+    <View>
+      <Text>ABC</Text>
+      <Text numberOfLines={5}>Prasoon</Text>
+      <Text>Gupta</Text>
     </View>
   );
 }
