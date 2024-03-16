2e8666903d195196de30a232cf140bb97b5ab5d6
Prasoon Gupta <PrasoonguptaOfficial@gmail.com>
2024-03-16 10:14:08 +0530

    Button UserInterface Component

--- App.tsx
+++ App.tsx
@@ -1,19 +1,12 @@
 import React from 'react';
-import {Text, View, StyleSheet} from 'react-native';
+import {View, Button} from 'react-native';
 
 function App(): React.JSX.Element {
   return (
     <View>
-      <Text style={styles.textBold}>ABC</Text>
+      <Button title="Click Me" onPress={() => {}} />
     </View>
   );
 }
-
-const styles = StyleSheet.create({
-  textBold: {
-    color: 'red',
-    fontSize: 20,
-  },
-});
 
 export default App;
