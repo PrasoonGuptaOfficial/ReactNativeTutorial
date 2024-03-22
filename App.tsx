252faf3d0fb01474eb45807d71c0a53c7e42746a
Prasoon Gupta <PrasoonguptaOfficial@gmail.com>
2024-03-23 00:52:18 +0530

    Stop Watch Timer Project Day 1

--- App.tsx
+++ App.tsx
@@ -1,60 +1,108 @@
-import React, {useState} from 'react';
-import {Button, PermissionsAndroid, Text, View} from 'react-native';
+/*
+STOP WATCH TIMER APPLICATION
+*/
+import React, {useReducer, useRef} from 'react';
+import {Pressable, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+
+interface State {
+  startTimerFlag: boolean;
+  stopTimerFlag: boolean;
+  resetTimerFlag: boolean;
+  sec: number;
+}
+
+interface Action {
+  type: string;
+}
+
+interface TimerIntervalIdRef {
+  current: number | undefined | NodeJS.Timeout;
+}
+
+const initialState: State = {
+  startTimerFlag: false,
+  stopTimerFlag: false,
+  resetTimerFlag: false,
+  sec: 0,
+};
+
+const reducer = (state: State, action: Action): State => {
+  switch (action.type) {
+    case 'start_timer':
+      return {
+        ...state,
+        sec: state.sec + 1,
+      };
+    case 'stop_timer':
+      return {
+        ...state,
+      };
+    case 'reset_timer':
+      return {
+        ...state,
+        sec: 0,
+      };
+    default:
+      return state;
+  }
+};
 
 function App(): React.JSX.Element {
-  const [accessed, setAccessed] = useState('');
-  const androidPermission = async () => {
-    try {
-      const access = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.CAMERA,
-        {
-          title: 'App Permission',
-          message: 'Demo',
-          buttonNeutral: 'Ask Me Later',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        },
-      );
-      if (access === PermissionsAndroid.RESULTS.GRANTED) {
-        setAccessed('Granted');
-      } else {
-        setAccessed('Denied');
-      }
-    } catch (e) {
-      throw new Error('Something went wrong');
-    }
+  const [state, dispatch] = useReducer(reducer, initialState);
+  let timerIntervalIdRef: TimerIntervalIdRef = useRef();
+  const onStartTimerHandler = () => {
+    timerIntervalIdRef.current = setInterval(() => {
+      dispatch({type: 'start_timer'});
+    }, 1);
   };
-  const bluetoothAndroidPermission = async () => {
-    try {
-      const access = await PermissionsAndroid.request(
-        PermissionsAndroid.PERMISSIONS.BLUETOOTH_SCAN,
-        {
-          title: 'App Permission',
-          message: 'Demo',
-          buttonNeutral: 'Ask Me Later',
-          buttonPositive: 'Ok',
-          buttonNegative: 'Cancel',
-        },
-      );
-      if (access === PermissionsAndroid.RESULTS.GRANTED) {
-        setAccessed('Granted');
-      } else {
-        setAccessed('Denied');
-      }
-    } catch (e) {
-      throw new Error('Something went wrong');
-    }
+  const onStopTimerHandler = () => {
+    clearInterval(timerIntervalIdRef.current);
+    dispatch({type: 'stop_timer'});
+  };
+  const onResetTimerHandler = () => {
+    clearInterval(timerIntervalIdRef.current);
+    dispatch({type: 'reset_timer'});
   };
   return (
-    <View>
-      <Text>{accessed}</Text>
-      <Button title="Camera Permission" onPress={androidPermission} />
-      <Button
-        title="Bluetooth Permission"
-        onPress={bluetoothAndroidPermission}
-      />
-    </View>
+    <SafeAreaView style={styles.mainAppView}>
+      <Text style={styles.mainAppText}>STOP WATCH TIMER APP</Text>
+      <View />
+      <Text style={styles.timerText}>{state.sec}</Text>
+      <Pressable onPress={onStartTimerHandler}>
+        <Text>Start</Text>
+      </Pressable>
+      <Pressable onPress={onStopTimerHandler}>
+        <Text>Stop</Text>
+      </Pressable>
+      <Pressable onPress={onResetTimerHandler}>
+        <Text>Reset</Text>
+      </Pressable>
+    </SafeAreaView>
   );
 }
+
+const styles = StyleSheet.create({
+  mainAppView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  mainAppText: {
+    fontSize: 16,
+    fontWeight: 'bold',
+    fontVariant: ['small-caps'],
+    color: 'black',
+    textAlign: 'center',
+    textDecorationLine: 'underline',
+    textDecorationColor: 'black',
+  },
+  timerText: {
+    fontSize: 16,
+    fontWeight: 'normal',
+    fontVariant: ['small-caps'],
+    color: 'black',
+    textAlign: 'center',
+  },
+});
 
 export default App;
