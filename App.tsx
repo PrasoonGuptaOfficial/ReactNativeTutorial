231057b3436195816d7f6fd15520006a81c0fa38
Prasoon Gupta <PrasoonguptaOfficial@gmail.com>
2024-03-29 22:06:24 +0530

    Stop Watch Timer Project Day 2

--- App.tsx
+++ App.tsx
@@ -1,14 +1,25 @@
 /*
 STOP WATCH TIMER APPLICATION
 */
-import React, {useReducer, useRef} from 'react';
-import {Pressable, SafeAreaView, StyleSheet, Text, View} from 'react-native';
+import React, {useEffect, useReducer, useRef, useState} from 'react';
+import {
+  AppState,
+  Pressable,
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+} from 'react-native';
 
 interface State {
   startTimerFlag: boolean;
   stopTimerFlag: boolean;
   resetTimerFlag: boolean;
+  millisec: number;
   sec: number;
+  min: number;
+  hr: number;
+  day: number;
 }
 
 interface Action {
@@ -23,24 +34,68 @@ const initialState: State = {
   startTimerFlag: false,
   stopTimerFlag: false,
   resetTimerFlag: false,
+  millisec: 0,
   sec: 0,
+  min: 0,
+  hr: 0,
+  day: 0,
 };
+
+let startMillisecond = 0,
+  startSecond = 0,
+  startMinute = 0,
+  startHour = 0,
+  startDay = 0;
 
 const reducer = (state: State, action: Action): State => {
   switch (action.type) {
     case 'start_timer':
+      startMillisecond = state.millisec + 1;
+      if (startMillisecond > 999) {
+        startMillisecond = 0;
+        startSecond = state.sec + 1;
+      }
+      if (startSecond > 59) {
+        startSecond = 0;
+        startMinute = state.min + 1;
+      }
+      if (startMinute > 59) {
+        startMinute = 0;
+        startHour = state.hr + 1;
+      }
+      if (startHour > 23) {
+        startHour = 0;
+        startDay = state.day + 1;
+      }
       return {
         ...state,
-        sec: state.sec + 1,
+        millisec: startMillisecond,
+        sec: startSecond,
+        min: startMinute,
+        hr: startHour,
+        day: startDay,
+        startTimerFlag: true,
+        stopTimerFlag: true,
+        resetTimerFlag: true,
       };
     case 'stop_timer':
       return {
         ...state,
+        startTimerFlag: false,
+        stopTimerFlag: false,
+        resetTimerFlag: true,
       };
     case 'reset_timer':
       return {
         ...state,
+        millisec: 0,
         sec: 0,
+        min: 0,
+        hr: 0,
+        day: 0,
+        startTimerFlag: false,
+        stopTimerFlag: false,
+        resetTimerFlag: false,
       };
     default:
       return state;
@@ -49,6 +104,34 @@ const reducer = (state: State, action: Action): State => {
 
 function App(): React.JSX.Element {
   const [state, dispatch] = useReducer(reducer, initialState);
+  const [appState, setAppState] = useState<string>(AppState.currentState);
+  useEffect(() => {
+    const onHandleAppStateChange = (nextAppState: string) => {
+      console.log('Prasoon => ', nextAppState, typeof nextAppState);
+      setAppState(nextAppState);
+    };
+    const subscription = AppState.addEventListener(
+      'change',
+      onHandleAppStateChange,
+    );
+    return () => {
+      subscription.remove();
+    };
+  }, []);
+  let appStatusText;
+  switch (appState) {
+    case 'active':
+      appStatusText = 'Active';
+      break;
+    case 'background':
+      appStatusText = 'Background';
+      break;
+    case 'inactive':
+      appStatusText = 'Inactive';
+      break;
+    default:
+      appStatusText = 'Unknown';
+  }
   let timerIntervalIdRef: TimerIntervalIdRef = useRef();
   const onStartTimerHandler = () => {
     timerIntervalIdRef.current = setInterval(() => {
@@ -63,20 +146,39 @@ function App(): React.JSX.Element {
     clearInterval(timerIntervalIdRef.current);
     dispatch({type: 'reset_timer'});
   };
+  const onTwoDigitConversion = (digit: number) => {
+    return digit.toLocaleString('en-US', {
+      minimumIntegerDigits: 2,
+      useGrouping: false,
+    });
+  };
   return (
     <SafeAreaView style={styles.mainAppView}>
       <Text style={styles.mainAppText}>STOP WATCH TIMER APP</Text>
       <View />
-      <Text style={styles.timerText}>{state.sec}</Text>
-      <Pressable onPress={onStartTimerHandler}>
-        <Text>Start</Text>
-      </Pressable>
-      <Pressable onPress={onStopTimerHandler}>
-        <Text>Stop</Text>
-      </Pressable>
-      <Pressable onPress={onResetTimerHandler}>
-        <Text>Reset</Text>
-      </Pressable>
+      <Text>{appStatusText}</Text>
+      <Text style={styles.timerText}>{`${onTwoDigitConversion(
+        state.day,
+      )}:${onTwoDigitConversion(state.hr)}:${onTwoDigitConversion(
+        state.min,
+      )}:${onTwoDigitConversion(state.sec)}:${onTwoDigitConversion(
+        state.millisec,
+      )}`}</Text>
+      {!state.startTimerFlag && (
+        <Pressable onPress={onStartTimerHandler}>
+          <Text>Start</Text>
+        </Pressable>
+      )}
+      {state.stopTimerFlag && (
+        <Pressable onPress={onStopTimerHandler}>
+          <Text>Stop</Text>
+        </Pressable>
+      )}
+      {state.resetTimerFlag && (
+        <Pressable onPress={onResetTimerHandler}>
+          <Text>Reset</Text>
+        </Pressable>
+      )}
     </SafeAreaView>
   );
 }
